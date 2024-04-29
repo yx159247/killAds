087fd1e4137c4ebd95ddb76653f5f9314f69d624
Yangxu <321740709@qq.com>
2024-04-29 09:58:56 +0800

    更新广告屏蔽规则及请求体处理逻辑

--- rewrite/js/boss-rewrite-req.js
+++ rewrite/js/boss-rewrite-req.js
@@ -4,6 +4,6 @@ var bossReq = $request.body;
 
 // 修改请求体
 // appKey=gyPinXiXGptx5TZf&userId=G31i0xmhCPrGgBrM8U0DgA%3D%3D&version=11.250 把版本号改成12.0.0
-bossReq = bossReq.replace(/version=(\d+\.\d+\.\d+)/, 'version=12.0.0');
-console.log(bossReq);
-$done(bossReq);
+var modifiedUrl = bossReq.replace(/version=11\.250/, "version=12.0.0")
+console.log(modifiedUrl);
+$done(modifiedUrl);
