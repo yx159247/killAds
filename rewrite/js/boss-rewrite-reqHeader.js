29b6447a6b7a50b6aef86018225abf3a5a1ecb03
Yangxu <321740709@qq.com>
2024-04-29 10:58:04 +0800

    更新请求头处理逻辑，添加请求路径输出

--- rewrite/js/boss-rewrite-reqHeader.js
+++ rewrite/js/boss-rewrite-reqHeader.js
@@ -5,7 +5,7 @@ console.log('修改前的请求头',modifiedHeaders['User-Agent']);
 
 // 修改请求头
 
-modifiedHeaders['User-Agent'] = 'BossZP/12.0.0 (iPhone; iOS 15.4.1; Scale/3.00)'
+modifiedHeaders['User-Agent'] = 'BossZP/12.060 (iPhone; iOS 15.4.1; Scale/3.00)'
 
 var modifiedPath = $request.url;
 console.log('修改后的请求头',modifiedHeaders['User-Agent']);
