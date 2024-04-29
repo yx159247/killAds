445a5b57d3bb6cd2b3d93018d0b6c17a0e7b83b3
Yangxu <321740709@qq.com>
2024-04-29 10:11:59 +0800

    更新广告屏蔽规则及请求处理逻辑

--- rewrite/js/boss-rewrite-req.js
+++ rewrite/js/boss-rewrite-req.js
@@ -3,7 +3,7 @@ var bossReq = $request.body;
 
 
 // 修改请求体
-// appKey=gyPinXiXGptx5TZf&userId=G31i0xmhCPrGgBrM8U0DgA%3D%3D&version=11.250 把版本号改成12.0.0
+
 var modifiedUrl = bossReq.replace(/version=11\.250/, "version=12.0.0")
 console.log(modifiedUrl);
 $done(modifiedUrl);
--- rewrite/js/boss-rewrite-reqHeader.js
+++ rewrite/js/boss-rewrite-reqHeader.js
@@ -3,7 +3,7 @@
 var modifiedHeaders = $request.headers;
 
 // 修改请求头
-// appKey=gyPinXiXGptx5TZf&userId=G31i0xmhCPrGgBrM8U0DgA%3D%3D&version=11.250 把版本号改成12.0.0
+
 modifiedHeaders['User-Agent'] = 'BossZP/12.0.0 (iPhone; iOS 15.4.1; Scale/3.00)'
 
 console.log(modifiedHeaders);
